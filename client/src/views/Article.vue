38b9eace01b570829ec893bd196a66efc04dc698
Wassim <wassim.bacha@adventori.com>
2025-02-07 12:52:02 +0100

    fixed articles list

--- client/src/views/Article.vue
+++ client/src/views/Article.vue
@@ -1,63 +1,65 @@
-<template>
-    <div class="container mt-5">
-      <h1 class="text-center">{{ article.title }}</h1>
-      <p class="text-center text-muted">Écrit par {{ article.author.username }}</p>
-      <p class="mt-3">{{ article.content }}</p>
-  
-      <div class="mt-4">
-        <h4>👍 {{ article.likes }} Likes</h4>
-      </div>
-  
-      <div class="mt-5">
-        <h3>Commentaires ({{ article.comments.length }})</h3>
-        <ul class="list-group">
-          <li v-for="comment in article.comments" :key="comment.id" class="list-group-item">
-            <strong>{{ comment.author.username }}:</strong> {{ comment.content }}
-          </li>
-        </ul>
-      </div>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from "vue";
-  import { useRoute } from "vue-router";
-  import { useQuery } from "@vue/apollo-composable";
-  import gql from "graphql-tag";
-  
-  // Récupérer l'ID de l'article depuis l'URL
-  const route = useRoute();
-  const articleId = route.params.id;
-  
-  // Définition de la requête GraphQL
-  const GET_ARTICLE = gql`
-    query GetArticle($id: ID!) {
-      article(id: $id) {
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import { useRoute } from "vue-router";
+import { useQuery } from "@vue/apollo-composable";
+import { gql } from "graphql-tag";
+
+const route = useRoute();
+const articleId = route.params.id as string;
+
+const GET_ARTICLE = gql`
+  query GetArticle($id: ID!) {
+    article(id: $id) {
+      id
+      title
+      content
+      likes
+      author {
+        username
+      }
+      comments {
         id
-        title
         content
-        likes
         author {
           username
         }
-        comments {
-          id
-          content
-          author {
-            username
-          }
-        }
       }
     }
-  `;
-  
-  const { result, loading, error } = useQuery(GET_ARTICLE, { id: articleId });
-  const article = ref(null);
-  
-  watch(result, (newResult) => {
-    if (newResult && newResult.article) {
-      article.value = newResult.article;
-    }
-  });
-  </script>
-  
+  }
+`;
+
+const { result, loading, error } = useQuery(GET_ARTICLE, { id: articleId });
+const article = ref<{ id: string; title: string; content: string; likes: number; author: { username: string }; comments: { id: string; content: string; author: { username: string } }[] } | null>(null);
+
+watchEffect(() => {
+  if (result.value) {
+    article.value = result.value.article;
+  }
+});
+</script>
+
+<template>
+  <div class="container mt-5">
+    <div v-if="loading" class="text-center">Chargement...</div>
+    <div v-else-if="error" class="text-center text-danger">{{ error }}</div>
+    <div v-else-if="article">
+      <h1 class="text-center">{{ article.title }}</h1>
+      <p class="text-muted text-center">Auteur : {{ article.author.username }}</p>
+      <p class="lead">{{ article.content }}</p>
+
+      <div class="mt-4">
+        <strong>👍 {{ article.likes?.toString() || "0" }} Likes</strong>
+      </div>
+
+      <hr />
+
+      <h3>Commentaires</h3>
+      <div v-if="article.comments.length > 0">
+        <div v-for="comment in article.comments" :key="comment.id" class="border p-3 my-2">
+          <p><strong>{{ comment.author.username }}</strong>: {{ comment.content }}</p>
+        </div>
+      </div>
+      <div v-else class="text-muted">Aucun commentaire pour cet article.</div>
+    </div>
+  </div>
+</template>
--- client/src/views/Articles.vue
+++ client/src/views/Articles.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, watchEffect } from "vue";
 import { useQuery } from "@vue/apollo-composable";
 import { gql } from "graphql-tag";
 
@@ -18,9 +18,9 @@ const GET_ARTICLES = gql`
 `;
 
 const { result, loading, error } = useQuery(GET_ARTICLES);
-const articles = ref<{ id: string; title: string; content: string; author: { username: string } }[]>([]);
+const articles = ref<{ id: string; title: string; content: string; likes: number; author: { username: string } }[]>([]);
 
-onMounted(() => {
+watchEffect(() => {
   if (result.value) {
     articles.value = result.value.articles;
   }
@@ -29,19 +29,21 @@ onMounted(() => {
 
 <template>
   <div class="container mt-5">
-    <h1 class="text-center">Liste des Articles</h1>
+    <h1 class="text-center">Articles</h1>
 
     <div v-if="loading" class="text-center">Chargement...</div>
     <div v-else-if="error" class="text-center text-danger">{{ error }}</div>
     <div v-else>
       <div v-for="article in articles" :key="article.id" class="card my-3">
         <div class="card-body">
-          <h5 class="card-title">{{ article.title }}</h5>
+          <router-link :to="'/article/' + article.id" class="text-decoration-none">
+            <h5 class="card-title">{{ article.title }}</h5>
+          </router-link>
           <p class="card-text">{{ article.content }}</p>
           <small class="text-muted">
             Auteur : {{ article.author.username }}
             <br>
-            👍 : {{ article.likes }}
+            👍 {{ article.likes?.toString() || "0" }} Likes
           </small>
         </div>
       </div>
